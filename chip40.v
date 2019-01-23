14a8dd2a6024d209b52f3bea06ee77c054944a74
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-01-23 09:29:47 +0100

    Dim leds for up40 test bench

--- chip40.v
+++ chip40.v
@@ -13,16 +13,29 @@ module chip (
         	.CLKHF(clk)
     	);
 
+	reg led0, led1, led2;
+
 	test_deflate_bench mytest (
     		clk,
     		o_led,
-    		led0_g,
-    		led1_b,
-    		led2_r
+    		led0,
+    		led1,
+    		led2
        );
 
-	assign LED_R = led2_r;
-	assign LED_G = led0_g;
-	assign LED_B = led1_b;
+       SB_RGBA_DRV rgb (
+		.RGBLEDEN (1'b1),
+		.RGB0PWM  (led0),
+		.RGB1PWM  (led1),
+		.RGB2PWM  (led2),
+		.CURREN   (1'b1),
+		.RGB0     (LED_R),
+		.RGB1     (LED_G),
+		.RGB2     (LED_B)
+	);
+	defparam RGB_DRIVER.CURRENT_MODE = "0b1";
+	defparam RGB_DRIVER.RGB0_CURRENT = "0b000001";
+	defparam RGB_DRIVER.RGB1_CURRENT = "0b000001";
+	defparam RGB_DRIVER.RGB2_CURRENT = "0b000001";
 
 endmodule
